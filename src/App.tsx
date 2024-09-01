8b7b3eb4590c60f92ec6c823fb2efa6a7d549e1f
beau <a02278030@aggies.usu.edu>
2024-09-01 15:54:58 -0600

    added eslint and project content to front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
-import Message from './Message'
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
+//import Message from './Message'
+
+const projectList = [
+  {
+    name: "Flask Backend",
+    description: "A Flask-based backend API for a drone delivery service.",
+    link: "https://github.com/beaumartin25/dronecones-api",
+  },
+  {
+    name: "Gas Prices Data Analysis Project",
+    description: "Analyzing trends in gas prices using Python and Pandas.",
+    link: "https://github.com/beaumartin25/Data_Analysis_Project",
+  },
+  // Add more projects as needed
+];
 
 function App() {
-  return <div><Message></Message></div>
+  return (
+    <Container>
+      <Typography
+        variant="h1"
+        sx={{ my: 4, textAlign: "center", color: "primary.main" }}
+      >
+        Beau Martin
+      </Typography>
+      <Typography variant="h2">Overview</Typography>
+      <Box
+        sx={{
+          pt: 4,
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "space-between",
+          gap: 4,
+        }}
+      >
+        {projectList.map((project, index) => (
+          <Paper key={index} elevation={3} sx={{ width: { xs: 1, md: 600 } }}>
+            <Box sx={{ m: 3 }}>
+              <Typography variant="h3">{project.name}</Typography>
+              <Typography sx={{ mt: 2 }}>{project.description}</Typography>
+              <a href={project.link}>
+                <Button variant="contained" color="secondary" sx={{ mt: 2 }}>
+                  Go to project
+                </Button>
+              </a>
+            </Box>
+          </Paper>
+        ))}
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
